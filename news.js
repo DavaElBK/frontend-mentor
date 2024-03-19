7d95a67d5192a074ae65e3f869dc82db5078229f
Dawit Bayou <dawitb21@gmail.com>
2024-03-19 21:26:18 +0300

    frontend project 1

---
+++ news.js
@@ -0,0 +1,8 @@
+var btncolor= document.querySelector('#one')
+    btncolor.addEventListener('mouseover',function(){
+        btncolor.style.background='red';
+}
+)
+btncolor.addEventListener('mouseout',function(){
+    btncolor.style.background='blue';
+})
